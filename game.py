d238db73622f96d9a79f8f8d58066f6fe7f5fb6c
lcqsigi <lcqsigi@gmail.com>
2022-07-12 19:54:00 -0400

    user input

--- game.py
+++ game.py
@@ -3,6 +3,11 @@ print("Welcome to rock, paper, and scissors")
 
 #user input
 
+user_input=input("select rock, paper, or scissors: ")
+
+print(f"you chose: '{user_input}' ")
+#print("you chose: ", user_input)
+
 #validate user input
 
 #computer choice
