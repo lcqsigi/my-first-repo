ed86009bc7ad393ef74a8fc6fe39aa3dffbb0738
lcqsigi <lcqsigi@gmail.com>
2022-07-12 19:47:57 -0400

    added instructions

--- game.py
+++ game.py
@@ -1,2 +1,21 @@
 
 print("Welcome to rock, paper, and scissors")
+
+#user input
+
+#validate user input
+
+#computer choice
+
+#determine the winner
+
+#display results
+
+#----
+#Welcome player one...
+#----
+#Please choose either rock, paper, or scissors: rock
+#You chose rock
+#The computer chose paper
+#----
+#The computer won...
