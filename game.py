f00c281f16589c7cdc1256900eb1ad9ca86584c1
lcqsigi <lcqsigi@gmail.com>
2022-07-12 20:10:21 -0400

    added computer input

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 print("Welcome to rock, paper, and scissors")
 
@@ -5,14 +6,47 @@ print("Welcome to rock, paper, and scissors")
 
 user_input=input("select rock, paper, or scissors: ")
 
-print(f"you chose: '{user_input}' ")
 #print("you chose: ", user_input)
+
+print(f"you chose: '{user_input}' ")
 
 #validate user input
 
 #computer choice
 
+list1=['rock','paper','scissors']
+
+computer_input=np.random.choice(list1)
+
+print(f"computer chose: '{computer_input}' ")
+
 #determine the winner
+
+if user_input == 'rock':
+    num_user = 0
+if user_input == 'paper':
+    num_user = 1
+if user_input == 'scissors':
+    num_user = 2
+
+if computer_input == 'rock':
+    num_computer = 0
+if computer_input == 'paper':
+    num_computer = 1
+if computer_input == 'scissors':
+    num_computer = 2
+
+#rock -- rock
+#rock -- paper
+#rock -- scissors
+
+#paper -- rock
+#paper -- paper
+#paper -- scissors
+
+#scissors -- rock
+#scissors -- paper
+#scissors -- scissors
 
 #display results
 
