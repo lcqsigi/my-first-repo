45ffda731df5543e1d5ce69f1003f91fec0887a6
lcqsigi <lcqsigi@gmail.com>
2022-07-12 20:53:20 -0400

    determined winner

--- game.py
+++ game.py
@@ -6,6 +6,8 @@ print("Welcome to rock, paper, and scissors")
 
 user_input=input("select rock, paper, or scissors: ")
 
+valid_options = ["rock","paper","scissors"]
+
 #print("you chose: ", user_input)
 
 print(f"you chose: '{user_input}' ")
@@ -13,6 +15,8 @@ print(f"you chose: '{user_input}' ")
 #validate user input
 
 #computer choice
+
+computer_input=""
 
 list1=['rock','paper','scissors']
 
@@ -36,19 +40,34 @@ if computer_input == 'paper':
 if computer_input == 'scissors':
     num_computer = 2
 
-#rock -- rock
-#rock -- paper
-#rock -- scissors
+#rock -- rock      0 0      --> draw
+#rock -- paper     0 1      --> paper wins
+#rock -- scissors  0 2      --> rock wins
 
-#paper -- rock
-#paper -- paper
-#paper -- scissors
+#paper -- rock     1 0      --> paper wins
+#paper -- paper    1 1      --> draw
+#paper -- scissors 1 2      --> scissors win
 
-#scissors -- rock
-#scissors -- paper
-#scissors -- scissors
+#scissors -- rock     2 0   --> rock wins
+#scissors -- paper    2 1   --> scissors win
+#scissors -- scissors 2 2   --> draw
 
 #display results
+
+if num_user == num_computer:
+    print("draw")
+
+if num_user > num_computer and num_user != 0:
+    print("you won")
+
+if num_computer > num_user and num_computer != 0:
+    print("computer won")
+
+if num_user == 0 and num_computer == 2:
+    print("you won")
+
+if num_computer == 0 and num_user == 2:
+    print("computer won")
 
 #----
 #Welcome player one...
